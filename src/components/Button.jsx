50c9ed61580ecba802dffb51c603e8ec050ac3d4
arpita-2 <dasguptaarpita1999@gmail.com>
2024-05-27 15:53:36 +0530

    two buttons

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <div>
+      <button className="btn">Asc</button>
+      <button className="btn">Dsc</button>
+    </div>
+  );
+}

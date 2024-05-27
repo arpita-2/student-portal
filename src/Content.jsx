db4bdcee79a0fd7e4e123420592939d240fb1783
arpita-2 <dasguptaarpita1999@gmail.com>
2024-05-27 15:47:30 +0530

    make cards for students

---
+++ src/Content.jsx
@@ -0,0 +1,53 @@
+import { dataOfStudents } from "./utils/mockdata";
+
+//navigation bar
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <nav className="nav">
+        <div className="main-title">
+          Student Portal
+        </div>
+
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export const StudentsData = (props) => {
+  //stuData is prop. Then did destructuring of prop.
+  const { studData } = props;
+  const { name, grade, percentage, sports, careerChoices } = studData;
+
+  return (
+    <div>
+      <card>
+        <h3>{name}</h3>
+        <h4>{sports.join(", ")}</h4>
+        <h4>{careerChoices.join(", ")}</h4>
+        <p>{grade}</p>
+        <p>{percentage}</p>
+      </card>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div className="content">
+      {dataOfStudents.map((student) => (
+        <StudentsData key={student} studData={student} />
+      ))}
+    </div>
+  );
+};
+
+export default Content;

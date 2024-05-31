50f953de82b45f070f2976f4e91e2804fda70755
arpita-2 <89323421+arpita-2@users.noreply.github.com>
2024-05-31 13:32:50 +0530

    radio buttons for grade filters

--- src/Content.jsx
+++ src/Content.jsx
@@ -48,6 +48,23 @@ export const Filter = () => {
           />
         </div>
 
+
+        <div className="grade-filter">
+          <h3>Grades Filter</h3>
+          <div>
+            <input type="radio" id="grade10" name="grade" value="10" />
+            <label htmlFor="grade10">10 Grade</label>
+          </div>
+          <div>
+            <input type="radio" id="grade11" name="grade" value="11" />
+            <label htmlFor="grade11">11 Grade</label>
+          </div>
+          <div>
+            <input type="radio" id="grade12" name="grade" value="12" />
+            <label htmlFor="grade12">12 Grade</label>
+          </div>
+        </div>
+
       </div>
       </div>
   )
